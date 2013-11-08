8053332541b1bb3b91c25bad8e1095f34e9b69b3
Andrew Lutton <andrew.lutton@vthitec.org>
2013-11-08 16:19:54 -0500

    Example!!!!

--- hello_world.java
+++ hello_world.java
@@ -5,7 +5,7 @@ public class Hello_World {
 	public static void main(String[] args) {
 		System.out.println("Hello WORLD!");
 		System.out.println("Java is great!");
-		System.out.println("Git is very useful");
+		System.out.println("Git IS very useful");
         
 	}
 
